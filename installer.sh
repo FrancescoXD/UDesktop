8eae6f7b60393f168ac4270a39d4dc78ac6b4575
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2019-05-25 23:42:29 +0200

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Selected boolean
+selectedDE=false
+
+printf "
+  _    _ _____            _    _              
+ | |  | |  __ \          | |  | |             
+ | |  | | |  | | ___  ___| | _| |_ ___  _ __  
+ | |  | | |  | |/ _ \/ __| |/ / __/ _ \| '_ \ 
+ | |__| | |__| |  __/\__ \   <| || (_) | |_) |
+  \____/|_____/ \___||___/_|\_\\__\___/| .__/ 
+                                       | |    
+                                       |_|    
+"
+echo ""
+echo "Welcome, this is a complete desktop environment installer for ArchLinux! See my github profile: https://github.com/FrancescoXD"
+echo ""
+# Starting
+echo "What desktop environment (DE) do you need?"
+echo "g - GNOME"
+echo "l - LXDE"
+echo "k - KDE Plasma"
+echo "x - XFCE"
+read desktopEnvironment
+
+# Check desktop environment
+if [ $desktopEnvironment == g ]; then
+	echo "Installing GNOME with display manager LXDM..."
+	echo "Note: default terminal is broken, so use termite!"
+	pacman -S xorg xorg-server gnome gnome-extra termite lxdm
+	# Enable display manager
+	systemctl enable lxdm
+	selectedDE=true
+elif [ $desktopEnvironment == l ]; then
+	echo "Installing LXDE with display manager LXDM..."
+	pacman -S xorg xorg-server lxde lxde-common lxsession desktop-file-utils gamin lxdm
+	# Enable display manager
+	systemctl enable lxdm
+	selectedDE=true
+elif [ $desktopEnvironment == k ]; then
+	echo "Installing KDE Plasma with display manager SDDM..."
+	pacman -S xorg xorg-server plasma sddm kde-applications lxdm
+	# Enable display manager
+	systemctl enable sddm
+	selectedDE=true
+elif [ $desktopEnvironment == x ]; then
+	echo "Installing XFCE with display manager LXDM..."
+	pacman -S xorg xorg-server xfce4 xfce4-goodies lxdm
+	# Enable display manager
+	systemctl enable lxdm
+	selectedDE=true
+else
+	echo "Error: you did not choose a valid desktop environment!"
+fi
+
+# Download extra stuff
+if [ $selectedDE == "true" ]; then
+	echo "Installing extra stuff..."
+	pacman -S vlc pulseaudio pulseaudio-alsa firefox termite gimp htop git vim
+	echo "Do you want YAY for AUR (recommended)? (yes/no)
+	read installYay
+	if [ $installYay == yes ]; then
+		git clone https://aur.archlinux.org/yay.git
+		cd yay
+		makepkg -si
+		cd ..
+	fi
+	echo ""
+	echo "Thanks for using this script!"
+fi
+
+echo ""
+echo "Thanks for using this script!"
